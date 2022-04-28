7b7cf63f753412e55abd99c6eda17d6b0f28645e
Roger <roger.mendez@norimaconsulting.com>
2022-04-28 13:02:51 +0800

    Own implementation of Store class

--- Store.java
+++ Store.java
@@ -9,19 +9,103 @@ public class Store {
         Scanner in = new Scanner(System.in);
         NumberFormat money = NumberFormat.getCurrencyInstance();
         Product[] inventory = new Product[productNames.length];
-        int op;
-
+        int op, prodId, amount;
 
         for (int i = 0; i < productNames.length; i++) {
-            inventory[i] = new Product(productNames[i], initialStock[i], price[i])
+            inventory[i] = new Product(productNames[i], initialStock[i], price[i]);
         }
 
         do {
-            System.out.println("Current Inventory");
-            System.out.format("%20s\t%20s\t%20s", "Product", "Stock", "Price");
-            for (int i = 0; i < productNames.length; i++) {
-                System.out.format("%20s\t%20s\t%20s", inventory[i].getName(), inventory[i].get);
+            System.out.print("\033[H\033[2J");  
+            System.out.flush();  
+            System.out.println("1. Display Inventory");
+            System.out.println("2. Add Product Quantity");
+            System.out.println("3. Enter Sales Transaction");
+            System.out.println("4. Display All Sales Transactions");
+            System.out.print("Enter your choice (0 to exit): ");
+            op = in.nextInt();
+            in.nextLine();
+
+            System.out.println();
+            switch (op) {
+                case 1:
+                    System.out.println("Current Inventory");
+                    System.out.format("%-20s\t%-20s\t%-20s\n", "Product", "Stock", "Price");
+                    for (int i = 0; i < productNames.length; i++) {
+                        inventory[i].displayInv();
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < productNames.length; i++) {
+                        System.out.format("%-2s. %-20s\n", (i + 1), inventory[i].getName());
+                    }
+
+                    System.out.print("Enter product number to increase quantity: ");
+                    prodId = (in.nextInt() - 1);
+                    in.nextLine();
+
+                    if (prodId > productNames.length || prodId < 0) {
+                        System.out.println("Invalid input, returning to main menu...");
+                    }
+                    else {
+                        System.out.print("Enter quantity of " + inventory[prodId].getName() + " to increase by: ");
+                        amount = in.nextInt();
+                        in.nextLine();
+                        
+                        if (amount > 0) {
+                            inventory[prodId].addQuantity(amount);
+                        } 
+                        else {
+                            System.out.println("Invalid input, returning to main menu...");
+                        }
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i < productNames.length; i++) {
+                        System.out.format("%-2s. %-20s\n", (i + 1), inventory[i].getName());
+                    }
+
+                    System.out.print("Enter product number to product to buy: ");
+                    prodId = (in.nextInt() - 1);
+                    in.nextLine();
+
+                    if (prodId > productNames.length || prodId < 0) {
+                        System.out.println("Invalid input, returning to main menu...");
+                    }
+                    else {
+                        System.out.print("Enter quantity of " + inventory[prodId].getName() + " to buy: ");
+                        amount = in.nextInt();
+                        in.nextLine();
+                        
+                        if (amount > inventory[prodId].getStock()) {
+                            System.out.println("Invalid input. Input must be less than or equal to current stock. Returning to main menu...");
+                        } 
+                        else if (amount < 0) {
+                            System.out.println("Invalid input, returning to main menu...");
+                        }
+                        else {
+                            inventory[prodId].sellProduct(amount);
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("Current Inventory");
+                    System.out.format("%-20s\t%-20s\t%-20s\n", "Product", "Amount Sold", "Revenue");
+                    for (int i = 0; i < productNames.length; i++) {
+                        System.out.format("%-20s\t%-20s\t%-20s\n", inventory[i].getName(), inventory[i].getSold(),
+                            money.format(inventory[i].getSale()));
+                    }
+                    break;
+                case 0:
+                    System.out.println("exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
             }
+            System.out.print("\nPress enter to continue...");
+            in.nextLine();
         } while(op != 0);
+
+        in.close();
     }
 }
