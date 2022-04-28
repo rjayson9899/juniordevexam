15aeee6c12a2ea93e9906f4422d52d28703d8465
Roger <roger.mendez@norimaconsulting.com>
2022-04-28 10:12:36 +0800

    Initial Commit

---
+++ Product.java
@@ -0,0 +1,61 @@
+public class Product {
+    private String name;
+    private int quantity;
+    private double price;
+    private int sold;
+
+    /*
+     * Constructor
+     */
+    public Product (String name, int quantity, double price) {
+        //Insert code here
+    }
+
+    /*
+     * Code to display inventory
+     */
+    public void displayInv() {
+        //Insert code here
+    }
+
+    /*
+     * Code to add quantity of inventory
+     */
+    public void addQuantity(int amount) {
+        //Insert code here
+    }
+
+    /*
+     * Deducts quantity based on input amount
+     * returns final price based on amount of
+     * goods sold
+     */
+    public double sellProduct(int amount) {
+        //Insert code here
+    }
+
+    /*
+     * Fetch the name of the product
+     * as output
+     */
+    public String getName() {
+        //Insert code here
+    }
+
+    /*
+     * Fetch the total amount of
+     * goods sold as output
+     */
+    public int getSold() {
+        //Insert code here
+    }
+
+    /*
+     * Computes for the total sales
+     * of the product based on the 
+     * amount sold and price
+     */
+    public int getSale() {
+        //Insert code here
+    }
+}
